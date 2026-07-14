85a479d6deb1c7b834b05f5f247ea74cb975e6b1
siceballos <si.ceballos@duocuc.cl>
2026-07-14 19:45:59 -0400

    se agrega una funcion dedicada a mostrar el menu

--- LibreriaElSaber.py
+++ LibreriaElSaber.py
@@ -1,2 +1,27 @@
 """Sistema para administrar un inventario de libros"""
 
+
+def mostrarMenu():
+    libereria = []
+    print("------------------------------")
+    print("LIBRERIA EL SABER")
+    print("1.- Agregar libro")
+    print("2.- Mostrar un libro")
+    print("3.- Eliminar libro")
+    print("4.- Listar inventario")
+    print("5.- Valorizar inventario")
+    print("6.- Salir")
+
+    if opc == 1:
+        print()
+    elif opc == 2:
+        print()
+    elif opc == 3:
+        print()
+    elif opc == 4:
+        print()
+    elif opc == 5:
+        print()
+    elif opc == 6:
+        print("Fin del programa")
+        break
