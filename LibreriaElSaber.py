f0425de22edeb60e8b0c71794af3f2b149b3ce9f
simon ceballos <si.ceballos@duocuc.cl>
2026-07-15 04:12:35 -0400

    se agrega las funciones de la opcion 2 y 3, las cuales requieren de una funcion dedicada a buscar un libro recibiendo como parametros el titulo y la libreria, ademas se declara la lista fuera del ciclo iterativo ya que no se estaban recopilando los datos de manera efectiva

--- LibreriaElSaber.py
+++ LibreriaElSaber.py
@@ -2,29 +2,37 @@
 
 
 def mostrarMenu():
-    libereria = []
-    print("------------------------------")
-    print("LIBRERIA EL SABER")
-    print("1.- Agregar libro")
-    print("2.- Mostrar un libro")
-    print("3.- Eliminar libro")
-    print("4.- Listar inventario")
-    print("5.- Valorizar inventario")
-    print("6.- Salir")
-
-    if opc == 1:
-        print()
-    elif opc == 2:
-        print()
-    elif opc == 3:
-        print()
-    elif opc == 4:
-        print()
-    elif opc == 5:
-        print()
-    elif opc == 6:
-        print("Fin del programa")
-        break
+    libreria = []
+    while True:
+        print("------------------------------------------")
+        print("LIBRERIA EL SABER")
+        print("1.- Agregar libro")
+        print("2.- Mostrar un libro")
+        print("3.- Eliminar libro")
+        print("4.- Listar inventario")
+        print("5.- Valorizar inventario")
+        print("6.- Salir")
+        while True:
+            try:
+                opc = int(input("Ingrese la opcion que desea: "))
+                if validarOpc(opc):
+                    break
+            except ValueError:
+                print("Formato no valido")
+                
+        if opc == 1:
+            agregarLibro(libreria)
+        elif opc == 2:
+            mostrarLibro(libreria)
+        elif opc == 3:
+            eliminarLibro(libreria)
+        elif opc == 4:
+            print()
+        elif opc == 5:
+            print()
+        elif opc == 6:
+            print("Fin del programa")
+            break
 
 def validarCodigo(codigo):
     if len(codigo) == 7 and codigo[0].isalpha() and codigo[1].isalpha() and codigo[2].isalnum() and codigo[3].isalnum() and codigo[4].isalnum() and codigo[5].isnumeric() and codigo[6].isnumeric():
@@ -41,6 +49,9 @@ def validarPrecio(precio):
 def validarTitulo(titulo):
     return titulo.strip()!=""
 
+def validarOpc(opc):
+    return opc > 0 and opc <= 6
+
 def agregarLibro(libreria):
     while True:
         codigo = input("Ingrese el codigo del libro: ")
@@ -55,7 +66,7 @@ def agregarLibro(libreria):
         if validarAño(año):
             break
     while True:
-        precio = int(input("Ingrese el precio del libro"))
+        precio = int(input("Ingrese el precio del libro: "))
         if validarPrecio(precio):
             break
     dic = {"codigo":codigo,
@@ -64,8 +75,37 @@ def agregarLibro(libreria):
            "precio":precio}
     libreria.append(dic)
         
-     
-    
-        
 
+def buscarLibro(titulo,libreria):
+    pos = -1
+    for i in range(len(libreria)):
+        if libreria[i]['titulo']==titulo:
+            pos = i
+            break
+    return pos
+
+def mostrarLibro(libreria):
+    titulo = input("Ingrese el titulo del libro que desea visualizar: ")
+    ubicacion = buscarLibro(titulo,libreria)
+    print("------------------------------------------")
+    if ubicacion > -1:
+        print(f"Titulo: {libreria[ubicacion]['titulo']}")
+        print(f"Codigo: {libreria[ubicacion]['codigo']}")
+        print(f"Año: {libreria[ubicacion]['año']}")
+        print(f"Precio: {libreria[ubicacion]['precio']}")
+    else:
+        print("Libro no encontrado.")
+    
+def eliminarLibro(libreria):
+    titulo = input("Ingrese el titulo del libro que desea eliminar: ")
+    ubicacion = buscarLibro(titulo,libreria)
+    if ubicacion > -1:
+        print("-------------------------------------")
+        print("Libro eliminado con exito")
+        libreria.pop(ubicacion)
+    else:
+        print("No valido, libro no encontrado.")
+
+
+mostrarMenu()
 
